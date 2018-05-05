9ade83f33ba31689f2f1e2f4bb7818a63961198b
ERIC KELLY <eakelly220@gmail.com>
2018-05-05 21:05:32 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -2,7 +2,7 @@ class Transferi
   attr_accessor :bankaccount, :sender, :recever, :status
   def initialize(sender, reciever, ammount)
     @sender = sender 
-    @reciever = receiver 
+    @receiver = receiver 
     @ammount = ammount 
     @status = "pending"
   end 
